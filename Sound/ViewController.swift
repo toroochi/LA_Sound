db45d164e65cba0ebec607dbb70430f879b959fd
yamaokana <kudanobo11@outlook.jp>
2022-08-18 18:03:26 +0900

    初めてのコミット

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -9,15 +9,40 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
 
     let drumSoundPlayer = try!AVAudioPlayer(data:NSDataAsset(name:"drumSound")!.data)
+    let pianoSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name: "pianoSound")!.data)
+    let guitarSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name: "guitarSound")!.data)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
+    }
+    @IBAction func  touchiUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drumImage"), for:.normal)
+    }
+    @IBAction func touchDownPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+    }
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    @IBAction func touchDownGuiarButton(){
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+    }
+    @IBAction func touchUpGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
     }
 
 }
